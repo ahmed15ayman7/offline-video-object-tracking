5dc1b6000c1ec0c4e876973dba77cdd5a8d3f4c0
ahmed15ayman7 <111033436+ahmed15ayman7@users.noreply.github.com>
2023-04-06 01:12:53 +0200

    Delete ButtonTypeMeals.js

--- src/components/ButtonTypeMeals.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ButtonTypeMeals = ({onclTypes,type}) => {
-  return (
-    <button className='btn btn-type' onClick={(e)=>onclTypes(e.target.textContent)}>{type}</button>
-  )
-}
